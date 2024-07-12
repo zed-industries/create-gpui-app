9f8ee7009d1390cf5a8a32c4b70099ca21bb2c9c
Swapnil Tripathi <swapnil06.st@gmail.com>
2024-07-12 13:58:21 +0530

    add workspace template
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use dirs::DEFAULT_TEMPLATES_DIR;
+use dirs::{DEFAULT_TEMPLATE_DIR, WORKSPACE_TEMPLATE_DIR};
 use include_dir::{Dir, DirEntry};
 use std::{fs, io, path::Path};
 mod dirs;
@@ -8,8 +8,11 @@ mod dirs;
 #[clap(version, about, long_about = None)]
 struct Args {
     /// Name of the new project
-    #[clap(short, long)]
+    #[clap(short, long, default_value = "gpui-app")]
     name: Option<String>,
+    /// Setup your project as a workspace
+    #[clap(short, long)]
+    workspace: bool,
 }
 
 fn copy_and_replace(
@@ -18,15 +21,19 @@ fn copy_and_replace(
     project_name: &str,
 ) -> io::Result<()> {
     const WORD_TO_REPLACE: &str = "PROJECT_NAME";
-    fs::create_dir_all(destination_path)?;
+    let new_destination_path = if destination_path.file_name().unwrap() == WORD_TO_REPLACE {
+        destination_path.with_file_name(project_name)
+    } else {
+        destination_path.to_owned()
+    };
+    fs::create_dir_all(&new_destination_path)?;
 
     for entry in source_dir.entries() {
         let relative_path = entry.path().strip_prefix(source_dir.path()).unwrap();
-        let entry_path = destination_path.join(relative_path);
+        let entry_path = new_destination_path.join(relative_path);
+        println!("{:?}", entry_path);
         match entry {
-            DirEntry::Dir(dir) => {
-                copy_and_replace(&dir, &entry_path, project_name)?;
-            }
+            DirEntry::Dir(dir) => copy_and_replace(&dir, &entry_path, project_name)?,
             DirEntry::File(file) => {
                 if let Some(content) = file.contents_utf8() {
                     let new_content = content.replace(WORD_TO_REPLACE, project_name);
@@ -43,15 +50,24 @@ fn copy_and_replace(
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    let project_name = args.name.unwrap_or_else(|| "gpui-app".to_string());
+    let project_name = args.name.unwrap();
     let project_path = Path::new(&project_name);
+    let is_workspace = args.workspace;
 
     if project_path.exists() {
         println!("'{}' already exists.", project_name);
         return Ok(());
     }
 
-    copy_and_replace(&DEFAULT_TEMPLATES_DIR, project_path, &project_name)?;
+    copy_and_replace(
+        if is_workspace {
+            &WORKSPACE_TEMPLATE_DIR
+        } else {
+            &DEFAULT_TEMPLATE_DIR
+        },
+        project_path,
+        &project_name,
+    )?;
 
     println!(
         "Successfully created new gpui app project '{}'",
---
+++ src/templates/workspace/README.md
@@ -0,0 +1,12 @@
+# PROJECT_NAME
+
+Created with Create GPUI App.
+
+- [`gpui`](https://www.gpui.rs/)
+- [GPUI documentation](https://github.com/zed-industries/zed/tree/main/crates/gpui/docs)
+- [GPUI examples](https://github.com/zed-industries/zed/tree/main/crates/gpui/examples)
+
+## Usage
+
+- Ensure Rust is installed - [Rustup](https://rustup.rs/)
+- Run your app with `cargo run`
