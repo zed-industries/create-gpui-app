fd05684e309678b057b2c6be4cab47712399f997
Swapnil Tripathi <swapnil06.st@gmail.com>
2024-07-12 10:18:13 +0530

    Refactor code to enable modular templates
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
-use dirs::DEFAULT_TEMPLATES_DIR;
-use include_dir::{Dir, DirEntry};
-use std::{fs, io, path::Path};
-mod dirs;
+use std::{
+    fs,
+    io::{self, Read},
+    path::Path,
+};
 
 #[derive(Parser, Debug)]
 #[clap(version, about, long_about = None)]
@@ -12,28 +13,26 @@ struct Args {
     name: Option<String>,
 }
 
-fn copy_and_replace(
-    source_dir: &Dir,
-    destination_path: &Path,
-    project_name: &str,
-) -> io::Result<()> {
+fn copy_and_replace(source: &Path, destination: &Path, project_name: &str) -> io::Result<()> {
     const WORD_TO_REPLACE: &str = "PROJECT_NAME";
-    fs::create_dir_all(destination_path)?;
 
-    for entry in source_dir.entries() {
-        let relative_path = entry.path().strip_prefix(source_dir.path()).unwrap();
-        let entry_path = destination_path.join(relative_path);
-        match entry {
-            DirEntry::Dir(dir) => {
-                copy_and_replace(&dir, &entry_path, project_name)?;
-            }
-            DirEntry::File(file) => {
-                if let Some(content) = file.contents_utf8() {
-                    let new_content = content.replace(WORD_TO_REPLACE, project_name);
-                    fs::write(&entry_path, new_content)?;
-                }
-            }
+    if source.is_dir() {
+        fs::create_dir_all(destination)?;
+
+        for entry in fs::read_dir(source)? {
+            let entry = entry?;
+            let entry_path = entry.path();
+            let relative_path = entry_path.strip_prefix(source).unwrap();
+            let dest_path = destination.join(relative_path);
+
+            copy_and_replace(&entry_path, &dest_path, project_name)?;
         }
+    } else if source.is_file() {
+        let mut content = String::new();
+        fs::File::open(source)?.read_to_string(&mut content)?;
+
+        let new_content = content.replace(WORD_TO_REPLACE, project_name);
+        fs::write(destination, new_content)?;
     }
 
     Ok(())
@@ -51,7 +50,11 @@ fn main() -> io::Result<()> {
         return Ok(());
     }
 
-    copy_and_replace(&DEFAULT_TEMPLATES_DIR, project_path, &project_name)?;
+    copy_and_replace(
+        Path::new("src/templates/default"),
+        project_path,
+        &project_name,
+    )?;
 
     println!(
         "Successfully created new gpui app project '{}'",
