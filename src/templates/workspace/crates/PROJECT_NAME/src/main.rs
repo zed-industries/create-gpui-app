0a090c090bba215902a1a922e0e62e9b8b1c00eb
Swapnil Tripathi <swapnil06.st@gmail.com>
2024-07-13 12:49:52 +0530

    update workspace template
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>

--- src/templates/workspace/crates/PROJECT_NAME/src/main.rs
+++ src/templates/workspace/crates/PROJECT_NAME/src/main.rs
@@ -7,33 +7,24 @@ struct HelloWorld {
 impl Render for HelloWorld {
     fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
         div()
-            .flex()
-            .bg(rgb(0x2e7d32))
-            .size(Length::Definite(Pixels(300.0).into()))
-            .justify_center()
-            .items_center()
-            .shadow_lg()
-            .border_1()
-            .border_color(rgb(0x0000ff))
-            .text_xl()
-            .text_color(rgb(0xffffff))
-            .child(format!("Hello, {}!", &self.text))
+        .flex()
+        .bg(rgb(0x2e7d32))
+        .size_full()
+        .justify_center()
+        .items_center()
+        .text_xl()
+        .text_color(rgb(0xffffff))
+        .child(format!("Hello, {}!", &self.text))
     }
 }
 
 fn main() {
     App::new().run(|cx: &mut AppContext| {
-        let bounds = Bounds::centered(None, size(px(600.0), px(600.0)), cx);
-        cx.open_window(
-            WindowOptions {
-                window_bounds: Some(WindowBounds::Windowed(bounds)),
-                ..Default::default()
-            },
-            |cx| {
-                cx.new_view(|_cx| HelloWorld {
-                    text: "World".into(),
-                })
-            },
-        );
+        cx.open_window(WindowOptions::default(), |cx| {
+            cx.new_view(|_cx| HelloWorld {
+                text: "World".into(),
+            })
+        })
+        .unwrap();
     });
 }
