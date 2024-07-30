af9926ca175557561a7c4dcfc1713b889890eaec
swaptr <83858160+swaptr@users.noreply.github.com>
2024-07-30 22:19:24 +0530

    Add workspace template (#9)
    * Refactor code to enable modular templates
    
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>
    
    * enable inclusion of templates  in binary
    
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>
    
    * add workspace template
    
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>
    
    * add ui to workspace template
    
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>
    
    ---------
    
    Signed-off-by: Swapnil Tripathi <swapnil06.st@gmail.com>

--- src/dirs.rs
+++ src/dirs.rs
@@ -1,3 +1,4 @@
 use include_dir::{include_dir, Dir};
 
-pub static DEFAULT_TEMPLATES_DIR: Dir<'_> = include_dir!("src/templates/default");
+pub static DEFAULT_TEMPLATE_DIR: Dir<'_> = include_dir!("src/templates/default");
+pub static WORKSPACE_TEMPLATE_DIR: Dir<'_> = include_dir!("src/templates/workspace");
