69ca22419ef18d54001bbd38cc1fbd4729261466
Nate Butler <iamnbutler@gmail.com>
2024-08-03 10:00:55 -0400

    Use relative dirs to prevent panic on publish

--- src/dirs.rs
+++ src/dirs.rs
@@ -1,4 +1,6 @@
 use include_dir::{include_dir, Dir};
 
-pub static DEFAULT_TEMPLATE_DIR: Dir<'_> = include_dir!("src/templates/default");
-pub static WORKSPACE_TEMPLATE_DIR: Dir<'_> = include_dir!("src/templates/workspace");
+pub static DEFAULT_TEMPLATE_DIR: Dir<'_> =
+    include_dir!("$CARGO_MANIFEST_DIR/src/templates/default");
+pub static WORKSPACE_TEMPLATE_DIR: Dir<'_> =
+    include_dir!("$CARGO_MANIFEST_DIR/src/templates/workspace");
