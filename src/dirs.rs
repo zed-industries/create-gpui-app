ae3a4d767d67fec451dcde51e8ffbaf5e2a0c1e0
Nate Butler <iamnbutler@gmail.com>
2024-08-05 16:33:17 -0400

    Revert "Use relative dirs to prevent panic on publish"
    This reverts commit 69ca22419ef18d54001bbd38cc1fbd4729261466.

--- src/dirs.rs
+++ src/dirs.rs
@@ -1,6 +1,4 @@
 use include_dir::{include_dir, Dir};
 
-pub static DEFAULT_TEMPLATE_DIR: Dir<'_> =
-    include_dir!("$CARGO_MANIFEST_DIR/src/templates/default");
-pub static WORKSPACE_TEMPLATE_DIR: Dir<'_> =
-    include_dir!("$CARGO_MANIFEST_DIR/src/templates/workspace");
+pub static DEFAULT_TEMPLATE_DIR: Dir<'_> = include_dir!("src/templates/default");
+pub static WORKSPACE_TEMPLATE_DIR: Dir<'_> = include_dir!("src/templates/workspace");
